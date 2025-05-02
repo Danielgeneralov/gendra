152e453195e98131607cf97eb9ad546841c0178e
Danielgeneralov <danikgeneralov@gmail.com>
2025-05-02 01:08:31 -0700

    srr fix, trying different config

--- src/app/quote/industry/page.tsx
+++ src/app/quote/industry/page.tsx
@@ -1,18 +1,6 @@
-"use client";
-
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
-
-// Lazy load the client UI component
-const IndustryQuotePage = dynamic(() => import("./client"), {
-  ssr: false,
-  loading: () => <div className="text-white text-center mt-10">Loading...</div>,
-});
+'use client';
+import Client from './client';
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div className="text-white text-center mt-10">Loading...</div>}>
-      <IndustryQuotePage />
-    </Suspense>
-  );
+  return <Client />;
 }
