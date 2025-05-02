336f568e338d795ee0b8a796c18cdf308c67e643
Danielgeneralov <danikgeneralov@gmail.com>
2025-05-02 00:33:48 -0700

    ClientLoader fix try number 1 billion

--- src/app/quote/industry/ClientLoader.tsx
+++ src/app/quote/industry/ClientLoader.tsx
@@ -1,7 +1,11 @@
 "use client";
+
 import dynamic from "next/dynamic";
 
-const IndustryQuotePage = dynamic(() => import("./client"), { ssr: false });
+// Dynamically import the actual UI with ssr: false
+const IndustryQuotePage = dynamic(() => import("./client"), {
+  ssr: false,
+});
 
 export default function ClientLoader() {
   return <IndustryQuotePage />;
