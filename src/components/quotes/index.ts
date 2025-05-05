65d1726a8dae05757bc0d21406037ee3e0ed10f2
Danielgeneralov <danikgeneralov@gmail.com>
2025-05-05 11:18:37 -0700

    Applied same logic to other industry forms

--- src/components/quotes/index.ts
+++ src/components/quotes/index.ts
@@ -1,4 +1,6 @@
 export { default as CNCMachiningForm } from './CNCMachiningForm';
 export { default as InjectionMoldingForm } from './InjectionMoldingForm';
 export { default as SheetMetalForm } from './SheetMetalForm';
+export { default as ElectronicsAssemblyForm } from './ElectronicsAssemblyForm';
+export { default as MetalFabricationForm } from './MetalFabricationForm';
 export { default as QuoteFormWrapper } from './QuoteFormWrapper'; 
